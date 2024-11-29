41b7356014cbc0746f2e77636eedf0abcb0ebda6
guoxi <1206189299@qq.com>
2024-11-29 12:09:19 +0800

    service验证

---
+++ src/test/java/com/o2o/service/AreaServiceTest.java
@@ -0,0 +1,17 @@
+package com.o2o.service;
+
+import com.o2o.BaseTest;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class AreaServiceTest extends BaseTest {
+
+    @Autowired
+    private AreaService areaService;
+
+    @Test
+    public void testGetAreaList () {
+
+    }
+
+}
